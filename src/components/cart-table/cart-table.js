b95d201092664d75b75757702955d4c860b1988e
padavanshh <shagidulin01061997@gmail.com>
2021-06-24 21:30:46 +0300

    start cart table

--- src/components/cart-table/cart-table.js
+++ src/components/cart-table/cart-table.js
@@ -1,20 +1,46 @@
 import React from 'react';
 import './cart-table.scss';
+import {connect} from "react-redux";
 
-const CartTable = () => {
+const CartTable = ({items, onDelete, onIncrease, onDecrease}) => {
     return (
         <>
             <div className="cart__title">Ваш заказ:</div>
             <div className="cart__list">
-                <div className="cart__item">
-                    <img src="https://static.1000.menu/img/content/21458/-salat-cezar-s-kr-salat-cezar-s-krevetkami-s-maionezom_1501173720_1_max.jpg" className="cart__item-img" alt="Cesar salad"></img>
-                    <div className="cart__item-title">Cesar salad</div>
-                    <div className="cart__item-price">12$</div>
-                    <div className="cart__close">&times;</div>
-                </div>
+                {
+                    items.map((item) => {
+                        const {url, title, id, price} = item;
+                        return (
+                            <div key={id} className="cart__item">
+                                <img src={url} className="cart__item-img" alt={title}></img>
+                                <div className="cart__item-title">{title}</div>
+                                <div className="cart__item-price">{price}$</div>
+                                <button onClick={() => onDecrease(id)} className="cart__dec">-</button>
+                                <button onClick={() => onIncrease(id)} className="cart__inc">+</button>
+                                <div onClick={() => onDelete(id)} className="cart__close">&times;</div>
+                            </div>)
+                    })
+                }
             </div>
         </>
     );
 };
 
-export default CartTable;
+const mapStateToProps = (state)=> {
+    return {
+        items: state.items
+    }
+};
+
+const mapDispatchToProps = () => {
+    return {
+        onIncrease: (id) => {
+            console.log('increase '+ id)
+        },
+        onDecrease: (id) => {console.log('decrease '+ id)},
+        onDelete: (id)=>{console.log('delete '+ id)}
+
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartTable);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,25 +1,45 @@
 const initialState = {
     menu: [],
     loading: true,
-    error: null
+    error: null,
+    items: [
+        {
+            "title": "Cesar salad",
+            "price": 12,
+            "url": "https://static.1000.menu/img/content/21458/-salat-cezar-s-kr-salat-cezar-s-krevetkami-s-maionezom_1501173720_1_max.jpg",
+            "category": "salads",
+            "id": 1
+        },
+        {
+            "title": "Pizza Margherita",
+            "price": 10,
+            "url": "https://www.ricettedalmondo.it/images/foto-ricette/p/17500-pizza-margherita.jpg",
+            "category": "pizza",
+            "id": 2
+        }
+    ],
+    total: 50
 };
 
 const reducer = (state=initialState, action) => {
     switch (action.type) {
         case 'FETCH_MENU_REQUEST':
             return {
+                ...state,
                 menu: [],
                 loading: true,
                 error: null
             };
         case 'FETCH_MENU_SUCCESS':
             return {
+                ...state,
                 menu: action.payload,
                 loading: false,
                 error: null
             };
         case 'FETCH_MENU_FAILURE':
             return {
+                ...state,
                 menu:[],
                 loading: false,
                 error: action.payload
